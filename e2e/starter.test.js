4d1f05435f007262b25a10ae0d64881333c40da1
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-06-04 22:53:37 -0300

    config detox

---
+++ e2e/starter.test.js
@@ -0,0 +1,16 @@
+import { device, element, by } from "detox"
+
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should show world screen after tap', async () => {
+    await element(by.id('world_button')).tap();
+    await expect(element(by.text('World!!!'))).toBeVisible();
+  });
+});
